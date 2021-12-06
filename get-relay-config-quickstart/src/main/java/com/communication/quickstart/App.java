a7e6cfd3de0e9db0bf61ef3fd48f6b3d7b45bc0c
Ari Zavala <arzavala@microsoft.com>
2021-12-06 11:03:45 -0800

    Update PR

--- get-relay-config-quickstart/src/main/java/com/communication/quickstart/App.java
+++ get-relay-config-quickstart/src/main/java/com/communication/quickstart/App.java
@@ -5,25 +5,30 @@ import com.azure.communication.identity.CommunicationIdentityClient;
 import com.azure.communication.identity.CommunicationIdentityClientBuilder;
 import com.azure.communication.networktraversal.*;
 import com.azure.communication.networktraversal.models.*;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.KurentoClient;
 import java.util.List;
 import java.lang.reflect.*;
 
 public class App 
 {
+        private static KurentoClient kurento;
+
     // You can find your connection string from your resource in the Azure portal
     private static String connectionString = "https://<RESOURCE_NAME>.communication.azure.com/;accesskey=<ACCESS_KEY>";
     private static CommunicationIdentityClient communicationIdentityClient = new CommunicationIdentityClientBuilder()
-        .connectionString(connectionString)
-        .buildClient();
-
+                    .connectionString(connectionString)
+                    .buildClient();
+            
     private static CommunicationRelayClient communicationRelayClient = new CommunicationRelayClientBuilder()
-        .connectionString(connectionString)
-        .buildClient();
+                    .connectionString(connectionString)
+                    .buildClient();
 
     public static void main(String[] args)
     {
         System.out.println("Azure Communication Services - NetworkTraversal Quickstart");
-
+        
         System.out.println("Getting a relay configuration");
         getRelayConfiguration();
 
@@ -37,48 +42,61 @@ public class App
     public static void getRelayConfiguration()
     {
         CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration();
-
+        
         System.out.println("Expires on:" + config.getExpiresOn());
         List<CommunicationIceServer> iceServers = config.getIceServers();
 
         for (CommunicationIceServer iceS : iceServers) {
-        System.out.println("URLS: " + iceS.getUrls());
-        System.out.println("Username: " + iceS.getUsername());
-        System.out.println("credential: " + iceS.getCredential());
-        System.out.println("RouteType: " + iceS.getRouteType());
+                System.out.println("URLS: " + iceS.getUrls());
+                System.out.println("Username: " + iceS.getUsername());
+                System.out.println("credential: " + iceS.getCredential());
+                System.out.println("RouteType: " + iceS.getRouteType());
+        }
+
+        // Then you can configure your WebRtcEndpoint to use TURN credentials
+        MediaPipeline pipeline = kurento.createMediaPipeline();
+        WebRtcEndpoint webRtcEndpoint = new WebRtcEndpoint.Builder(pipeline).build();
+        
+        CommunicationIceServer iceServerToUse = iceServers.get(0);
+        String urlToUse = iceServerToUse.getUrls().get(0);
+
+        // Format for URL must be user:password@ipaddress:port
+        // We remove the Stun/TURN word from the URL
+        String ipAndPort = urlToUse.substring(4, urlToUse.length());
+
+        webRtcEndpoint.setTurnUrl(iceServerToUse.getUsername()+ ":" + iceServerToUse.getCredential() + "@" + ipAndPort);
     }
-}
 
     public static void getRelayConfigurationUsingIdentity()
     {
-        CommunicationUserIdentifier user = communicationIdentityClient.createUser();
-        System.out.println("User id: " + user.getId());
+            CommunicationUserIdentifier user = communicationIdentityClient.createUser();
+            System.out.println("User id: " + user.getId());
 
-        CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(user);
+            CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(user);
+            
+            System.out.println("Expires on:" + config.getExpiresOn());
+            List<CommunicationIceServer> iceServers = config.getIceServers();
 
-        System.out.println("Expires on:" + config.getExpiresOn());
-        List<CommunicationIceServer> iceServers = config.getIceServers();
-
-        for (CommunicationIceServer iceS : iceServers) {
-            System.out.println("URLS: " + iceS.getUrls());
-            System.out.println("Username: " + iceS.getUsername());
-            System.out.println("credential: " + iceS.getCredential());
-            System.out.println("RouteType: " + iceS.getRouteType());
-        }
+            for (CommunicationIceServer iceS : iceServers) {
+                    System.out.println("URLS: " + iceS.getUrls());
+                    System.out.println("Username: " + iceS.getUsername());
+                    System.out.println("credential: " + iceS.getCredential());
+                    System.out.println("RouteType: " + iceS.getRouteType());
+            }
     }
 
     public static void getRelayConfigurationUsingRouteType()
     {
         CommunicationRelayConfiguration config = communicationRelayClient.getRelayConfiguration(RouteType.NEAREST);
-
+        
         System.out.println("Expires on:" + config.getExpiresOn());
         List<CommunicationIceServer> iceServers = config.getIceServers();
 
         for (CommunicationIceServer iceS : iceServers) {
-            System.out.println("URLS: " + iceS.getUrls());
-            System.out.println("Username: " + iceS.getUsername());
-            System.out.println("credential: " + iceS.getCredential());
-            System.out.println("RouteType: " + iceS.getRouteType());
+                System.out.println("URLS: " + iceS.getUrls());
+                System.out.println("Username: " + iceS.getUsername());
+                System.out.println("credential: " + iceS.getCredential());
+                System.out.println("RouteType: " + iceS.getRouteType());
         }
     }
 }

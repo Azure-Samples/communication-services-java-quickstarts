3ce60bf02a0f04347628626af80019ed6bfd39be
Fang Chen <16232002+fangchen0601@users.noreply.github.com>
2021-12-20 11:15:34 -0800

    update incomingcallquickstart

--- IncomingCallQuickstart/src/main/java/com/communication/incomingcallquickstart/App.java
+++ IncomingCallQuickstart/src/main/java/com/communication/incomingcallquickstart/App.java
@@ -1,13 +1,24 @@
 package com.communication.incomingcallquickstart;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+
+@SpringBootApplication
 public class App 
 {
+    final static String url = "http://localhost:9007";
+    final static String serverPort = "9007";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication application = new SpringApplication(App.class);
+        application.setDefaultProperties(Collections.singletonMap("server.port", serverPort));
+        application.run(args);
+
+        // Logger.logMessage(Logger.MessageType.INFORMATION, "Starting ACS InComing Call Sample App ");
+
+        // Get configuration properties
+        ConfigurationManager configurationManager = ConfigurationManager.getInstance();
+        configurationManager.loadAppSettings();
     }
 }

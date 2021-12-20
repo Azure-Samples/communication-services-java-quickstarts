2759795367ed871b5298c1a96e1a3cdca807d3aa
Fang Chen <16232002+fangchen0601@users.noreply.github.com>
2021-12-20 11:18:31 -0800

    new project incomingcallsample

---
+++ incomingcallsample/src/main/java/com/communication/incomingcallsample/IncomingcallsampleApplication.java
@@ -0,0 +1,13 @@
+package com.communication.incomingcallsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomingcallsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomingcallsampleApplication.class, args);
+	}
+
+}

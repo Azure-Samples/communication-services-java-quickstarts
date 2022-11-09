916ad9f6b4693dd05a1778e3476c5d1ad7ed7eb1
Franko Morales <fmorales@microsoft.com>
2022-11-09 12:08:04 -0800

    Adding Speec

--- CallAutomation_AppointmentReminder/src/main/java/com/communication/appointmentreminder/utitilities/Constants.java
+++ CallAutomation_AppointmentReminder/src/main/java/com/communication/appointmentreminder/utitilities/Constants.java
@@ -3,7 +3,7 @@ package com.communication.appointmentreminder.utitilities;
 public final class Constants {
     public static final String SAMPLE_NAME = "appointmentreminder";
     public static final String APP_ROOT = "src/main/java/com/communication/" + SAMPLE_NAME;
+    public static final String AUDIO_FILES_ROUTE = APP_ROOT + "/audio/";
     public static final String PROPERTIES_FILE_ROUTE = APP_ROOT + "/config.properties";
-    public static final String AUDIO_FILES_ROUTE = "src/main/resources/static/audio/";
     public static final String CALLBACK_PATH = "/api/outboundcall/callback";
 }

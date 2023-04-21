fbc61c5b3b27b8a33a36fc038be0c29cc4e682d0
Jan Novak <jannovak@microsoft.com>
2023-04-21 05:36:27 +0200

    refactor

--- DirectRouting/src/main/java/com/communication/quickstart/App.java
+++ DirectRouting/src/main/java/com/communication/quickstart/App.java
@@ -15,86 +15,75 @@ public class App {
         System.out.println("Azure Communication Services - Direct Routing Quickstart");
 
         String connectionString = "https://<RESOURCE_NAME>.communication.azure.com/;accesskey=<ACCESS_KEY>";
-        SipRoutingAsyncClient sipRoutingAsyncClient = new SipRoutingClientBuilder()
+        SipRoutingAsyncClient client = new SipRoutingClientBuilder()
                 .connectionString(connectionString)
                 .buildAsyncClient();
 
-        try {
-            listTrunksAndRoutes(sipRoutingAsyncClient);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Listing failed");
-        }
-
-        try {
-            setTrunksAndRoutes(sipRoutingAsyncClient);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Setting failed");
-        }
-
-        try {
-            listTrunksAndRoutes(sipRoutingAsyncClient);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Listing failed");
-        }
-
-        try {
-            updateTrunk(sipRoutingAsyncClient);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Setting failed");
-        }
-
-        try {
-            listTrunksAndRoutes(sipRoutingAsyncClient);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Listing failed");
-        }
+        listTrunksAndRoutes(client);
+        setTrunksAndRoutes(client);
+        listTrunksAndRoutes(client);
+        updateTrunk(client);
+        listTrunksAndRoutes(client);
 
         System.out.println("Finish");
     }
 
     private static void listTrunksAndRoutes(SipRoutingAsyncClient client) {
+        System.out.println("---------------------");
         System.out.println("Listing trunks");
-        List<SipTrunk> trunks = requireNonNull(client.listTrunks().byPage().blockFirst()).getValue();
-        for (SipTrunk trunk : trunks) {
-            System.out.printf(" %s:%d%n", trunk.getFqdn(), trunk.getSipSignalingPort());
-        }
-        if (trunks.isEmpty()) {
-            System.out.println(" There are no SIP trunks.");
-        }
+        try {
+            List<SipTrunk> trunks = requireNonNull(client.listTrunks().byPage().blockFirst()).getValue();
+            for (SipTrunk trunk : trunks) {
+                System.out.printf(" %s:%d%n", trunk.getFqdn(), trunk.getSipSignalingPort());
+            }
+            if (trunks.isEmpty()) {
+                System.out.println(" There are no SIP trunks.");
+            }
 
-        System.out.println("Listing routes");
-        List<SipTrunkRoute> routes = requireNonNull(client.listRoutes().byPage().blockFirst()).getValue();
-        for (SipTrunkRoute route : routes) {
-            System.out.printf(" %s: \"%s\" -> %s%n", route.getName(), route.getNumberPattern(), String.join(", ", route.getTrunks()));
-        }
-        if (routes.isEmpty()) {
-            System.out.println(" There are no voice routes.");
+            System.out.println("Listing routes");
+            List<SipTrunkRoute> routes = requireNonNull(client.listRoutes().byPage().blockFirst()).getValue();
+            for (SipTrunkRoute route : routes) {
+                System.out.printf(" %s: \"%s\" -> %s%n", route.getName(), route.getNumberPattern(), String.join(", ", route.getTrunks()));
+            }
+            if (routes.isEmpty()) {
+                System.out.println(" There are no voice routes.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Listing failed");
         }
     }
 
     private static void setTrunksAndRoutes(SipRoutingAsyncClient client) {
+        System.out.println("---------------------");
         System.out.println("Setting trunks");
-        client.setTrunksWithResponse(asList(
-                new SipTrunk("sbc.us.contoso.com", 1234),
-                new SipTrunk("sbc.eu.contoso.com", 1234)
-        )).block();
+        try {
+            client.setTrunksWithResponse(asList(
+                    new SipTrunk("sbc.us.contoso.com", 1234),
+                    new SipTrunk("sbc.eu.contoso.com", 1234)
+            )).block();
 
-        System.out.println("Setting routes");
-        client.setRoutes(asList(
-                new SipTrunkRoute("UsRoute", "^\\+1(\\d{10})$")
-                        .setTrunks(asList("sbc.us.contoso.com")),
-                new SipTrunkRoute("DefaultRoute", "^\\+\\d+$")
-                        .setTrunks(asList("sbc.us.contoso.com", "sbc.eu.contoso.com"))
-        )).block();
+            System.out.println("Setting routes");
+            client.setRoutes(asList(
+                    new SipTrunkRoute("UsRoute", "^\\+1(\\d{10})$")
+                            .setTrunks(asList("sbc.us.contoso.com")),
+                    new SipTrunkRoute("DefaultRoute", "^\\+\\d+$")
+                            .setTrunks(asList("sbc.us.contoso.com", "sbc.eu.contoso.com"))
+            )).block();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Setting failed");
+        }
     }
 
     private static void updateTrunk(SipRoutingAsyncClient client) {
+        System.out.println("---------------------");
         System.out.println("Updating trunk");
-        client.setTrunk(new SipTrunk("sbc.us.contoso.com", 9876)).block();
+        try {
+            client.setTrunk(new SipTrunk("sbc.us.contoso.com", 9876)).block();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Updating failed");
+        }
     }
 }

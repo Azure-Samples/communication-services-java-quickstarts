1d5f0b178eae683727dcca426c7cf5dfb5186d75
Durgesh Suryawanshi <v-durgeshs@microsoft.com>
2024-05-10 17:25:29 +0530

    added test changes for audio streaming.

--- CallAutomation_OutboundCalling/src/main/java/com/communication/callautomation/ProgramSample.java
+++ CallAutomation_OutboundCalling/src/main/java/com/communication/callautomation/ProgramSample.java
@@ -10,6 +10,8 @@ import com.azure.communication.common.PhoneNumberIdentifier;
 import com.azure.communication.identity.implementation.models.CommunicationErrorResponseException;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
+
+import ch.qos.logback.core.util.COWArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +26,11 @@ public class ProgramSample {
     private AppConfig appConfig;
     private CallAutomationClient client;
 
-    private String MainMenu =
-    """ 
-    Hello this is Contoso Bank, we’re calling in regard to your appointment tomorrow 
-    at 9am to open a new account. Please say confirm if this time is still suitable for you or say cancel 
-    if you would like to cancel this appointment.
-    """;
+    private String MainMenu = """
+            Hello this is Contoso Bank, we’re calling in regard to your appointment tomorrow
+            at 9am to open a new account. Please say confirm if this time is still suitable for you or say cancel
+            if you would like to cancel this appointment.
+            """;
     private String confirmLabel = "Confirm";
     private String cancelLabel = "Cancel";
     private String confirmedText = "Thank you for confirming your appointment tomorrow at 9am, we look forward to meeting with you.";
@@ -63,51 +64,100 @@ public class ProgramSample {
                     event.getServerCallId());
 
             if (event instanceof CallConnected) {
-                // (Optional) Add a Microsoft Teams user to the call.  Uncomment the below snippet to enable Teams Interop scenario.
+                // (Optional) Add a Microsoft Teams user to the call. Uncomment the below
+                // snippet to enable Teams Interop scenario.
                 // client.getCallConnection(callConnectionId).addParticipant(
-                //       new CallInvite(new MicrosoftTeamsUserIdentifier(appConfig.getTargetTeamsUserId()))
-                //                 .setSourceDisplayName("Jack (Contoso Tech Support)"));
-                
+                // new CallInvite(new
+                // MicrosoftTeamsUserIdentifier(appConfig.getTargetTeamsUserId()))
+                // .setSourceDisplayName("Jack (Contoso Tech Support)"));
+
+                StartMediaStreamingOptions startMediaStreamingOptions = new StartMediaStreamingOptions();
+                startMediaStreamingOptions.setOperationCallbackUrl(
+                        " https://d785-2409-40c2-4012-6347-2d46-8f6c-a036-8287.ngrok-free.app");
+                startMediaStreamingOptions.setOperationContext("startMediaStreamingContext");
+                // without options.
+                // client.getCallConnection(callConnectionId)
+                // .getCallMedia().startMediaStreaming();
+
+                // With options.
+                client.getCallConnection(callConnectionId)
+                        .getCallMedia().startMediaStreamingWithResponse(startMediaStreamingOptions, Context.NONE);
+
                 // prepare recognize tones
-                startRecognizingWithChoiceOptions(callConnectionId, MainMenu, appConfig.getTargetphonenumber(), "mainmenu");
-            }
-            else if (event instanceof RecognizeCompleted) {
+                startRecognizingWithChoiceOptions(callConnectionId, MainMenu, appConfig.getTargetphonenumber(),
+                        "mainmenu");
+            } else if (event instanceof MediaStreamingStarted) {
+                log.info("Media Streaming Started");
+                MediaStreamingStarted acsEvent = (MediaStreamingStarted) event;
+                log.info("*****Media Streaming Status --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getMediaStreamingStatus());
+                log.info("*****Media Streaming Status Details --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getMediaStreamingStatusDetails());
+                log.info("*****Media Streaming Content --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getContentType());
+                log.info("*****MediaOperationContext --> " + acsEvent.getOperationContext());
+            } else if (event instanceof MediaStreamingStopped) {
+                log.info("Media Streaming Stopped");
+                MediaStreamingStopped acsEvent = (MediaStreamingStopped) event;
+                log.info("*****Media Streaming Status --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getMediaStreamingStatus());
+                log.info("*****Media Streaming Status Details --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getMediaStreamingStatusDetails());
+                log.info("*****Media Streaming Content --> "
+                        + acsEvent.getMediaStreamingUpdateResult().getContentType());
+                log.info("*****MediaOperationContext --> " + acsEvent.getOperationContext());
+            } else if (event instanceof MediaStreamingFailed) {
+                log.info("Media Streaming Failed");
+                MediaStreamingFailed acsEvent = (MediaStreamingFailed) event;
+                log.info("Message --> " + acsEvent.getResultInformation().getMessage());
+                log.info("Code --> " + acsEvent.getResultInformation().getCode());
+            } else if (event instanceof RecognizeCompleted) {
+                // without options.
+                // client.getCallConnection(callConnectionId)
+                // .getCallMedia().stopMediaStreaming();
+
+                StopMediaStreamingOptions stopMediaStreamingOptions = new StopMediaStreamingOptions();
+                stopMediaStreamingOptions
+                        .setOperationCallbackUrl("https://d785-2409-40c2-4012-6347-2d46-8f6c-a036-8287.ngrok-free.app");
+
+                // with options
+                client.getCallConnection(callConnectionId)
+                        .getCallMedia().stopMediaStreamingWithResponse(stopMediaStreamingOptions, Context.NONE);
+
                 log.info("Recognize Completed event received");
-                RecognizeCompleted acsEvent = (RecognizeCompleted) event; 
+                RecognizeCompleted acsEvent = (RecognizeCompleted) event;
                 var choiceResult = (ChoiceResult) acsEvent.getRecognizeResult().get();
                 String labelDetected = choiceResult.getLabel();
                 String phraseDetected = choiceResult.getRecognizedPhrase();
-                log.info("Recognition completed, labelDetected=" + labelDetected + ", phraseDetected=" + phraseDetected + ", context=" + event.getOperationContext());
-                String textToPlay = labelDetected.equals(confirmLabel) ? confirmedText  : cancelText;
+                log.info("Recognition completed, labelDetected=" + labelDetected + ", phraseDetected=" + phraseDetected
+                        + ", context=" + event.getOperationContext());
+                String textToPlay = labelDetected.equals(confirmLabel) ? confirmedText : cancelText;
                 handlePlay(callConnectionId, textToPlay);
-            }
-            else if(event instanceof RecognizeFailed ) {
+            } else if (event instanceof RecognizeFailed) {
                 log.error("Received failed event: {}", ((CallAutomationEventBaseWithReasonCode) event)
                         .getResultInformation().getMessage());
                 var recognizeFailedEvent = (RecognizeFailed) event;
-                var context =recognizeFailedEvent.getOperationContext();
-                if(context != null && context.equals(retryContext)){
+                var context = recognizeFailedEvent.getOperationContext();
+                if (context != null && context.equals(retryContext)) {
                     handlePlay(callConnectionId, noResponse);
-                }
-                else
-                {
+                } else {
                     var resultInformation = recognizeFailedEvent.getResultInformation();
                     log.error("Encountered error during recognize, message={}, code={}, subCode={}",
-                    resultInformation.getMessage(),
-                    resultInformation.getCode(),
-                    resultInformation.getSubCode());
+                            resultInformation.getMessage(),
+                            resultInformation.getCode(),
+                            resultInformation.getSubCode());
 
                     var reasonCode = recognizeFailedEvent.getReasonCode();
                     String replyText = reasonCode == ReasonCode.Recognize.PLAY_PROMPT_FAILED ||
-                        reasonCode == ReasonCode.Recognize.INITIAL_SILENCE_TIMEOUT ? customerQueryTimeout : 
-                        reasonCode== ReasonCode.Recognize.INCORRECT_TONE_DETECTED ? invalidAudio: 
-                        customerQueryTimeout;
-                    
-                        // prepare recognize tones
-                    startRecognizingWithChoiceOptions(callConnectionId, replyText, appConfig.getTargetphonenumber(), retryContext);
-                } 
-            }
-            else if(event instanceof PlayCompleted || event instanceof PlayFailed) {
+                            reasonCode == ReasonCode.Recognize.INITIAL_SILENCE_TIMEOUT ? customerQueryTimeout
+                                    : reasonCode == ReasonCode.Recognize.INCORRECT_TONE_DETECTED ? invalidAudio
+                                            : customerQueryTimeout;
+
+                    // prepare recognize tones
+                    startRecognizingWithChoiceOptions(callConnectionId, replyText, appConfig.getTargetphonenumber(),
+                            retryContext);
+                }
+            } else if (event instanceof PlayCompleted || event instanceof PlayFailed) {
                 log.info("Received Play Completed event. Terminating call");
                 hangUp(callConnectionId);
             }
@@ -117,13 +167,27 @@ public class ProgramSample {
 
     private String createOutboundCall() {
         try {
+
+            MediaStreamingOptions streamingOptions = new MediaStreamingOptions(
+                    "wss://145a-2409-40c2-4012-6347-2d46-8f6c-a036-8287.ngrok-free.app",
+                    MediaStreamingTransport.WEBSOCKET,
+                    MediaStreamingContentType.AUDIO,
+                    MediaStreamingAudioChannel.MIXED,
+                    false);
+
             PhoneNumberIdentifier caller = new PhoneNumberIdentifier(appConfig.getCallerphonenumber());
             PhoneNumberIdentifier target = new PhoneNumberIdentifier(appConfig.getTargetphonenumber());
             CallInvite callInvite = new CallInvite(target, caller);
             CreateCallOptions createCallOptions = new CreateCallOptions(callInvite, appConfig.getCallBackUri());
-            CallIntelligenceOptions callIntelligenceOptions = new CallIntelligenceOptions().setCognitiveServicesEndpoint(appConfig.getCognitiveServiceEndpoint());
+            CallIntelligenceOptions callIntelligenceOptions = new CallIntelligenceOptions()
+                    .setCognitiveServicesEndpoint(appConfig.getCognitiveServiceEndpoint());
             createCallOptions = createCallOptions.setCallIntelligenceOptions(callIntelligenceOptions);
+            createCallOptions = createCallOptions.setMediaStreamingConfiguration(streamingOptions);
             Response<CreateCallResult> result = client.createCallWithResponse(createCallOptions, Context.NONE);
+            // result.getValue().getCallConnectionProperties().getMediaStreamingSubscription().getState();
+            // log.info("Media subscription state:--> " +
+            // result.getValue().getCallConnectionProperties()
+            // .getMediaStreamingSubscription().getState().toString());
             return result.getValue().getCallConnectionProperties().getCallConnectionId();
         } catch (CommunicationErrorResponseException e) {
             log.error("Error when creating call: {} {}",
@@ -133,40 +197,42 @@ public class ProgramSample {
         }
     }
 
-    private void handlePlay(final String  callConnectionId, String textToPlay) {
+    private void handlePlay(final String callConnectionId, String textToPlay) {
         var textPlay = new TextSource()
-                .setText(textToPlay) 
+                .setText(textToPlay)
                 .setVoiceName("en-US-NancyNeural");
 
         client.getCallConnection(callConnectionId)
-        .getCallMedia()
-        .playToAll(textPlay);
+                .getCallMedia()
+                .playToAll(textPlay);
     }
 
-    private void startRecognizingWithChoiceOptions(final String callConnectionId, final String content, final String targetParticipant, final String context)
-    {
+    private void startRecognizingWithChoiceOptions(final String callConnectionId, final String content,
+            final String targetParticipant, final String context) {
         var playSource = new TextSource().setText(content).setVoiceName("en-US-NancyNeural");
 
-        var recognizeOptions = new CallMediaRecognizeChoiceOptions(new PhoneNumberIdentifier(targetParticipant),  getChoices())
-            .setInterruptCallMediaOperation(false)
-            .setInterruptPrompt(false)
-            .setInitialSilenceTimeout(Duration.ofSeconds(10))
-            .setPlayPrompt(playSource)
-            .setOperationContext(context);
+        var recognizeOptions = new CallMediaRecognizeChoiceOptions(new PhoneNumberIdentifier(targetParticipant),
+                getChoices())
+                .setInterruptCallMediaOperation(false)
+                .setInterruptPrompt(false)
+                .setInitialSilenceTimeout(Duration.ofSeconds(10))
+                .setPlayPrompt(playSource)
+                .setOperationContext(context);
 
         client.getCallConnection(callConnectionId)
-        .getCallMedia()
-        .startRecognizing(recognizeOptions);
+                .getCallMedia()
+                .startRecognizing(recognizeOptions);
     }
 
-    private List<RecognitionChoice> getChoices(){
+    private List<RecognitionChoice> getChoices() {
         var choices = Arrays.asList(
-            new RecognitionChoice().setLabel(confirmLabel).setPhrases(Arrays.asList("Confirm", "First", "One")).setTone(DtmfTone.ONE),
-            new RecognitionChoice().setLabel(cancelLabel).setPhrases(Arrays.asList("Cancel", "Second", "Two")).setTone(DtmfTone.TWO)
-            );
-            return choices;
+                new RecognitionChoice().setLabel(confirmLabel).setPhrases(Arrays.asList("Confirm", "First", "One"))
+                        .setTone(DtmfTone.ONE),
+                new RecognitionChoice().setLabel(cancelLabel).setPhrases(Arrays.asList("Cancel", "Second", "Two"))
+                        .setTone(DtmfTone.TWO));
+        return choices;
     }
-    
+
     private void hangUp(final String callConnectionId) {
         try {
             client.getCallConnection(callConnectionId).hangUp(true);
@@ -182,6 +248,7 @@ public class ProgramSample {
         CallAutomationClient client;
         try {
             client = new CallAutomationClientBuilder()
+                    .endpoint("https://x-pma-uswe-04.plat.skype.com:6448")
                     .connectionString(appConfig.getConnectionString())
                     .buildClient();
             return client;
